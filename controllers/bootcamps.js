b52af10d1a2e03fde667994a9cb7f71971550a4f
maxim3andr3 <maxim3andr3@gmail.com>
2019-11-25 22:34:37 +0100

    Handling erros

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,4 +1,5 @@
 const Bootcamp = require('../models/Bootcamp')
+const ErrorResponse = require('../utils/errorResponse')
 
 // @desc      Get all bootcamps
 // @route     GET /api/v1/bootcamps
@@ -11,7 +12,7 @@ exports.getBootcamps = async (req, res, next) => {
       .status(200)
       .json({ success: true, count: bootcamps.length, data: bootcamps })
   } catch (err) {
-    res.status(400).json({ success: false })
+    next(err)
   }
 }
 
@@ -23,12 +24,14 @@ exports.getBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findById(req.params.id)
 
     if (!bootcamp) {
-      return res.status(400).json({ success: false })
+      return next(
+        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+      )
     }
 
     res.status(200).json({ success: true, data: bootcamp })
   } catch (err) {
-    res.status(400).json({ success: false })
+    next(err)
   }
 }
 
@@ -41,7 +44,7 @@ exports.createBootcamp = async (req, res, next) => {
 
     res.status(201).json({ success: true, data: bootcamp })
   } catch (err) {
-    res.status(400).json({ success: false })
+    next(err)
   }
 }
 
@@ -56,12 +59,14 @@ exports.updateBootcamp = async (req, res, next) => {
     })
 
     if (!bootcamp) {
-      return res.status(400).json({ success: false })
+      return next(
+        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+      )
     }
 
     res.status(200).json({ success: true, data: bootcamp })
   } catch (err) {
-    res.status(400).json({ success: false })
+    next(err)
   }
 }
 
@@ -73,11 +78,13 @@ exports.deleteBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id)
 
     if (!bootcamp) {
-      return res.status(400).json({ success: false })
+      return next(
+        new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+      )
     }
 
     res.status(200).json({ success: true, data: {} })
   } catch (err) {
-    res.status(400).json({ success: false })
+    next(err)
   }
 }
