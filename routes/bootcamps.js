3872038a7a84bf2460ec2ff53a804494e87cd370
maxim3andr3 <maxim3andr3@gmail.com>
2019-11-29 17:30:51 +0100

    Adding photo upload

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -8,6 +8,7 @@ const {
   updateBootcamp,
   deleteBootcamp,
   getBootcampsInRadius,
+  bootcampPhotoUpload,
 } = require('../controllers/bootcamps');
 
 // Include other resource routers
@@ -17,6 +18,8 @@ const courseRouter = require('./courses');
 router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
+
+router.route('/:id/photo').put(bootcampPhotoUpload);
 
 router
   .route('/')
