b2a10c55a680e4b2078aa454d14d3c25ce6b2001
maxim3andr3 <maxim3andr3@gmail.com>
2019-12-02 16:22:59 +0100

    Creating admin users CRUD

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -12,11 +12,11 @@ const {
 } = require('../controllers/bootcamps');
 
 const Bootcamp = require('../models/Bootcamp');
-const advancedResults = require('../middleware/advancedResults');
 
 // Include other resource routers
 const courseRouter = require('./courses');
 
+const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
 
 // Re-route into other resource routers
